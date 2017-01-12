7bcac9e8ab0f0747ab053a1d9f7f82c22dc7df90
Grigory Fomin <grigory.fomin@sravni.ru>
2017-01-12 11:42:30 +0300

    2

---
+++ addressbook-web-test/GroupData.cs
@@ -0,0 +1,36 @@
+﻿namespace addressbook_web_test
+{
+    public class GroupData
+    {
+        private string _name;
+        private string _header;
+        private string _footer;
+
+        public GroupData(string name, string header = "", string footer = "")
+        {
+            this._name = name;
+            this._header = header;
+            this._footer = footer;
+        }
+
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+
+        public string Header
+        {
+            get { return _header; }
+            set { _header = value; }
+        }
+
+        public string Footer
+        {
+            get { return _footer; }
+            set { _footer = value; }
+        }
+    }
+}
